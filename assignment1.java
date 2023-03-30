3ba57ab47a45cc1f813dfa32027d24fa7675e688
OmkarWalunj17 <121512085+OmkarWalunj17@users.noreply.github.com>
2023-03-30 12:09:31 +0530

    Adding Java Code to the Repository
    Uploading the Java code for Assignment 1.

---
+++ assignment1.java
@@ -0,0 +1,16 @@
+import java.util.*;     
+class assignment1{
+    public static void main(String args[]){  
+
+Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+System.out.print("Enter a number: ");  
+int a= sc.nextInt();
+
+if (a>0){
+    System.out.println("Number is Positive !");
+}
+else{
+    System.out.println("Number is Negative !");
+}
+}  
+}
