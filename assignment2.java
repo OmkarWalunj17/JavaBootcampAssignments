b56da596bc3993742e48e680d703145350da9591
Omkar Walunj <121512085+OmkarWalunj17@users.noreply.github.com>
2023-03-30 22:26:09 +0530

    Assignment 2 Java Source Code
    Uploaded the Java Source Code for Sum Function of two integers.

---
+++ assignment2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class assignment2{
+    public static void main(String args[]){
+       
+        Scanner sc = new Scanner(System.in);
+
+        int num1, num2;
+        num1 = sc.nextInt();
+        System.out.println("First Number is: " + num1);
+
+        num2 = sc.nextInt();
+        System.out.println("Second Number is: " + num2);
+        
+        int result = num1 + num2;
+        System.out.println("Sum is: " + result);      
+    }
+};
+
+
+
+
+
+
+    
+
+        
+
+
+        
